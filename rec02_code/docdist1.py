d386e50bece6c5ba546d090cfd8b13134761dde7
michal <michallis95@vp.pl>
2018-04-17 18:28:20 +0200

    Python cost model

--- rec02_code/docdist1.py
+++ rec02_code/docdist1.py
@@ -27,12 +27,13 @@
 # where:
 #    inner_product(x,y) = x1*y1 + x2*y2 + ... xn*yn
 #    norm(x) = sqrt(inner_product(x,x))
-
+import cProfile
 import math
-    # math.acos(x) is the arccosine of x.
-    # math.sqrt(x) is the square root of x.
+# math.acos(x) is the arccosine of x.
+# math.sqrt(x) is the square root of x.
 
 import sys
+
 
 def read_file(filename):
     """ 
@@ -43,8 +44,9 @@ def read_file(filename):
         f = open(filename, 'r')
         return f.readlines()
     except IOError:
-        print "Error opening or reading input file: ",filename
+        print("Error opening or reading input file: ", filename)
         sys.exit()
+
 
 def get_words_from_line_list(L):
     """
@@ -58,6 +60,7 @@ def get_words_from_line_list(L):
         word_list = word_list + words_in_line
     return word_list
 
+
 def get_words_from_string(line):
     """
     Return a list of the words in the given input string,
@@ -67,21 +70,24 @@ def get_words_from_string(line):
     Output: a list of strings 
               (each string is a sequence of alphanumeric characters)
     """
-    word_list = []          # accumulates words in line
-    character_list = []     # accumulates characters in word
+    word_list = []  # accumulates words in line
+    character_list = []  # accumulates characters in word
     for c in line:
         if c.isalnum():
             character_list.append(c)
-        elif len(character_list)>0:
+        # for preventing a double whitespace here
+        elif len(character_list) > 0:
             word = "".join(character_list)
             word = word.lower()
             word_list.append(word)
             character_list = []
-    if len(character_list)>0:
+    # take care of the last word
+    if len(character_list) > 0:
         word = "".join(character_list)
         word = word.lower()
         word_list.append(word)
     return word_list
+
 
 def count_frequency(word_list):
     """
@@ -94,9 +100,10 @@ def count_frequency(word_list):
                 entry[1] = entry[1] + 1
                 break
         else:
-            L.append([new_word,1])
+            L.append([new_word, 1])
     return L
-    
+
+
 def word_frequencies_for_file(filename):
     """
     Return alphabetically sorted list of (word,frequency) pairs 
@@ -105,9 +112,11 @@ def word_frequencies_for_file(filename):
     line_list = read_file(filename)
     word_list = get_words_from_line_list(line_list)
     freq_mapping = count_frequency(word_list)
+    print(freq_mapping)
     return freq_mapping
 
-def inner_product(L1,L2):
+
+def inner_product(L1, L2):
     """
     Inner product between two vectors, where vectors
     are represented as lists of (word,freq) pairs.
@@ -122,27 +131,35 @@ def inner_product(L1,L2):
                 sum += count1 * count2
     return sum
 
-def vector_angle(L1,L2):
+
+def vector_angle(L1, L2):
     """
     The input is a list of (word,freq) pairs, sorted alphabetically.
 
     Return the angle between these two vectors.
     """
-    numerator = inner_product(L1,L2)
-    denominator = math.sqrt(inner_product(L1,L1)*inner_product(L2,L2))
-    return math.acos(numerator/denominator)
+    numerator = inner_product(L1, L2)
+    denominator = math.sqrt(inner_product(L1, L1) * inner_product(L2, L2))
+    return math.acos(numerator / denominator)
+
 
 def main():
-    if len(sys.argv) != 3:
-        print "Usage: docdist1.py filename_1 filename_2"
-    else:
-        filename_1 = sys.argv[1]
-        filename_2 = sys.argv[2]
-        sorted_word_list_1 = word_frequencies_for_file(filename_1)
-        sorted_word_list_2 = word_frequencies_for_file(filename_2)
-        distance = vector_angle(sorted_word_list_1,sorted_word_list_2)
-        print "The distance between the documents is: %0.6f (radians)"%distance
+    # if len(sys.argv) != 3:
+    #     print("Usage: docdist1.py filename_1 filename_2")
+    # else:
+    # read the document
+    filename_1 = sys.argv[1]
+    filename_2 = sys.argv[2]
+    filename_1 = 'example1.txt'
+    filename_2 = 'example2.txt'
+    # compute document vector
+    sorted_word_list_1 = word_frequencies_for_file(filename_1)
+    sorted_word_list_2 = word_frequencies_for_file(filename_2)
+
+    # calculate vector angle
+    distance = vector_angle(sorted_word_list_1, sorted_word_list_2)
+    print("The distance between the documents is: {} (radians)".format(distance))
+
 
 if __name__ == "__main__":
-    import cProfile
     cProfile.run("main()")
